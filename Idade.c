f92b23f61fae5a006682f66e6bef0bbdd455e9b7
marcusvthome <71160051+marcusvthome@users.noreply.github.com>
2020-09-13 16:39:57 -0300

    Commit inicial

---
+++ Idade.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main () {
+
+	int AnoAtual = 0;
+	
+	AnoNasc, Idade = 0
+	
+	char Nome[10];
+	 
+	printf("Informe o seu Nome:\n");
+	scanf("%s", Nome);
+	 
+	printf("Informe o ano de nascimento:\n");
+	scanf("%", &AnoNasc);
+	
+	printf("Informe o Ano Atual:\n");
+	scanf("%d", &Anoatual);
+	
+	idade =  AnoNas - AnoAtual;
+	
+	printf("Sua idade: %f", Idade);
+	
+}
